592682d00dcf364e1b90ece285c232eeb7a019bc
erdbeerschoggi <M.Piotrowska.inf24@stud.bbbaden.ch>
2025-01-17 11:22:39 +0100

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,127 +1,128 @@
-namespace WinFormsApp2
+namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        public int spieler = 2;
+        public int zug = 0;
+        public int s1 = 0;
+        public int s2 = 0;
+        public int sd = 0;
+
+        bool isDraw()
+        {
+            if ((zug == 9) && (isWinner() == false))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        bool isWinner()
+        {
+            //horizontal
+            if ((b1.Text == b2.Text) && (b2.Text == b3.Text) && b1.Text != "")
+                return true;
+            if ((b4.Text == b5.Text) && (b5.Text == b6.Text) && b4.Text != "")
+                return true;
+            if ((b7.Text == b8.Text) && (b8.Text == b9.Text) && b7.Text != "")
+                return true;
+            //vertikal
+            if ((b1.Text == b4.Text) && (b4.Text == b7.Text) && b1.Text != "")
+                return true;
+            if ((b2.Text == b5.Text) && (b5.Text == b8.Text) && b2.Text != "")
+                return true;
+            if ((b3.Text == b6.Text) && (b6.Text == b9.Text) && b3.Text != "")
+                return true;
+            //vertikal
+            if ((b1.Text == b5.Text) && (b5.Text == b9.Text) && b1.Text != "")
+                return true;
+            if ((b3.Text == b5.Text) && (b5.Text == b7.Text) && b3.Text != "")
+                return true;
+            else
+                return false;
+        }
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        String CalTotal;
-        int num1;
-        int num2;
-        String option;
-        int result;
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            lbl4.Text = "X:" + s1;
+            lbl2.Text = "O:" + s1;
+            lbl3.Text = "Gleichstand:" + s1;
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void buttonClick(object sender, EventArgs e)
         {
-            Display.Text = Display.Text + "1";
-        }
+            Button button = (Button)sender;
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Display.Text = Display.Text + "2";
-        }
+            if (button.Text == "")
+            {
+                if (spieler % 2 == 0)
+                {
+                    button.Text = "X";
+                    spieler++;
+                    zug++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    spieler++;
+                    zug++;
+                }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Display.Text = Display.Text + "3";
-        }
+                if (isDraw() == true)
+                {
+                    MessageBox.Show("Unentschieden");
+                    sd++;
+                    NeuesSpiel();
+                }
+                if (isWinner() == true)
+                {
+                    if (button.Text == "X")
+                    {
+                        MessageBox.Show("X hat Gewonnen");
+                        s1++;
+                        NeuesSpiel();
+                    }
+                    else
+                    {
+                        MessageBox.Show("O hat Gewonnen");
+                        s2++;
+                        NeuesSpiel();
+                    }
+                }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Display.Text = Display.Text + "4";
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            Display.Text = Display.Text + "5";
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            Display.Text = Display.Text + "6";
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            Display.Text = Display.Text + "7";
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            Display.Text = Display.Text + "8";
+            }
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            Display.Text = Display.Text + "9";
+            NeuesSpiel();
+        }
+
+        public void NeuesSpiel()
+        {
+            spieler = 2;
+            zug = 0;
+            b1.Text = b2.Text = b3.Text = b4.Text = b5.Text = b6.Text = b7.Text = b8.Text = b9.Text = "";
+
+            lbl4.Text = "X:" + s1;
+            lbl2.Text = "O:" + s2;
+            lbl3.Text = "Gleichstand:" + sd;
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-            Display.Text = Display.Text + "0";
-        }
-
-        private void button12_Click(object sender, EventArgs e)
-        {
-            option = "+";
-            num1 = int.Parse(Display.Text);
-            Display.Clear();
-        }
-
-        private void button13_Click(object sender, EventArgs e)
-        {
-            option = "-";
-            num1 = int.Parse(Display.Text);
-            Display.Clear();
-        }
-
-        private void button14_Click(object sender, EventArgs e)
-        {
-            option = "*";
-            num1 = int.Parse(Display.Text);
-            Display.Clear();
-        }
-
-        private void button15_Click(object sender, EventArgs e)
-        {
-            option = "/";
-            num1 = int.Parse(Display.Text);
-            Display.Clear();
-        }
-
-        private void button17_Click(object sender, EventArgs e)
-        {
-            num2 = int.Parse(Display.Text);
-
-            if (option == "+")
-                result = num1 + num2;
-
-            if (option == "-")
-                result = num1 - num2;
-
-            if (option == "*")
-                result = num1 * num2;
-
-            if (option == "/")
-                result = num1 / num2;
-
-            Display.Text = result + "";
-
-        }
-
-        private void button16_Click(object sender, EventArgs e)
-        {
-            Display.Clear();
-            result = (0);
-            num1 = (0);
-            num2 = (0);
+            s1 = s2 = sd = 0;
+            NeuesSpiel();
         }
     }
 }
